02a016369477105e34cb97d462f3598b9a95bf6a
mikkelvs <mikkelvs@gmail.com>
2024-06-05 15:20:39 +0200

    set basePath to /contentful

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     images: {
       unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/contentful' : '/contentful',
   
 };
 
